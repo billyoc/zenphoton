fa2f32aedbfd91548a9755ca178596d818ca13a0
Micah Elizabeth Scott <beth@scanlime.org>
2013-04-12 18:41:36 -0700

    New build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+# Worker thread
+(
+    cat html/src/header.js
+    (
+        coffee -c -p html/src/rayworker.coffee
+    ) | jsmin
+) > html/rayworker.js
+
+# Main file
+(
+    cat html/src/header.js
+    (
+        cat html/src/jquery-1.9.1.min.js
+        cat html/src/jquery.hotkeys.js
+        (
+            cat html/src/zen-renderer.coffee
+            cat html/src/zen-widgets.coffee
+            cat html/src/zen-ui.coffee
+            cat html/src/zen-setup.coffee
+        ) | coffee -p -s
+    ) | jsmin
+) > html/zenphoton.js
