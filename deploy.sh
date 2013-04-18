2a5b948e26cab1c83f953a08c80a9a8625dd6b4e
Micah Elizabeth Scott <beth@scanlime.org>
2013-04-18 15:30:48 -0700

    Add iOS logos to deployment list

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,10 @@ FILES="\
     html/missing.html       \
     html/favicon.gif        \
     html/favicon.ico        \
+    html/logo-57.png        \
+    html/logo-72.png        \
+    html/logo-114.png       \
+    html/logo-144.png       \
     html/roboto.ttf         "
 
 scp $FILES scanlime@scanlime.org:~/zenphoton.com/
